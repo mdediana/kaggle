e66cd9905abe90dd0f0ca2a562f7d390823f047b
Mauricio De Diana <mdediana@gmail.com>
2020-01-24 17:40:10 -0300

    Search hyperparameters, add RandomForest

--- titanic/model.py
+++ titanic/model.py
@@ -1,15 +1,15 @@
 import argparse
 import sys
 import logging
-from statistics import mean
 from itertools import product
+import math
 
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier, RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from xgboost import XGBClassifier
 
@@ -86,29 +86,36 @@ def train(training_filename, model_class, model_params={}):
 
 
 def search_xgboost_hyperparams(training_filename):
+    # https://machinelearningmastery.com/configure-gradient-boosting-algorithm/
     xgboost_params = {
-        'max_depth': [2, 3, 4, 6, 8, 10],
-        'learning_rate': [1, 0.5, 0.1, 0.05, 0.025, 0.001],
-        'n_estimators': [50, 100, 200, 500, 1000],
-        'gamma': [0.05, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0],
-        'min_child_weight': [1, 3, 5, 7],
-        'subsample': [0.5, 0.75, 1.0],
-        'colsample_bytree': [0.4, 0.6, 0.8, 1.0],
-        'reg_alpha': [0, 0.1, 0.5, 1.0],
-        'reg_lambda': [0.01, 0.1, 1.0],
+        # 'max_depth': [2, 3, 4, 6, 8, 10],
+        'max_depth': [3],
+        # 'learning_rate': [1, 0.5, 0.1, 0.05, 0.025, 0.001],
+        'learning_rate': [0.07, 0.05],
+        # 'n_estimators': [50, 100, 200, 500, 1000],
+        'n_estimators': [200, 500, 1000, 2000],
+        # 'gamma': [0.05, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0],
+        # 'min_child_weight': [1, 3, 5, 7],
+        # 'subsample': [0.5, 0.75, 1.0],
+        # 'colsample_bytree': [0.4, 0.6, 0.8, 1.0],
+        # 'reg_alpha': [0, 0.1, 0.5, 1.0],
+        # 'reg_lambda': [0.01, 0.1, 1.0],
     }
     combinations = list(product(*xgboost_params.values()))
     logger.info('Number of combinations: %d', len(combinations))
-    best_score = 1
+    best_score = 0
     best_params = []
     for i, combination in enumerate(combinations, 1):
         params = dict(zip(xgboost_params.keys(), combination))
-        _, scores = train(training_filename, params)
-        score = mean(scores)
-        if score <= best_score:
+        _, scores = train(training_filename, XGBClassifier, params)
+        score = scores.mean()
+        std = scores.std()
+        if score > best_score:
             best_score = score
+            best_params = [params]
+        elif math.isclose(score, best_score, rel_tol=0.0001):
             best_params.append(params)
-        logger.debug('%d/%d (%s): %f %s | best score: %f', i, len(combinations), params, score, scores, best_score)
+        logger.debug(f'{i}/{len(combinations)} {params}: {score}+/-{std} {scores} | best score: {best_score}')
     logger.info('Best score: %f', best_score)
     logger.info('Best params:')
     for params in best_params:
@@ -125,7 +132,7 @@ def predict(pipeline, test_filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', help='Command to run', choices=['train', 'predict'], default='train')
+    parser.add_argument('command', help='Command to run', choices=['train', 'predict', 'search'], default='train')
     parser.add_argument('--training-set-file', help='Training set file', default='train.csv')
     parser.add_argument('--predict-file', help='Test set file', default='test.csv')
     args = parser.parse_args()
@@ -139,27 +146,32 @@ if __name__ == '__main__':
 
     MODEL_PARAMS = {
         XGBClassifier: {
-            'max_depth': 2,
-            'learning_rate': 1,
-            'n_estimators': 50,
-            'gamma': 0.05,
-            'min_child_weight': 3,
-            'subsample': 0.5,
-            'colsample_bytree': 0.8,
-            'reg_alpha': 1.0,
-            'reg_lambda': 0.01
+            'max_depth': 3,
+            'learning_rate': 0.05,
+            'n_estimators': 500,
+            # 'gamma': 0.05,
+            # 'min_child_weight': 3,
+            # 'subsample': 0.5,
+            # 'colsample_bytree': 0.8,
+            # 'reg_alpha': 1.0,
+            # 'reg_lambda': 0.01
         },
         GradientBoostingClassifier: {
             # 'n_estimators': 500,
-            # # 'learning_rate': 0.01,
-            # # 'max_depth': 5,
+            # 'learning_rate': 0.1,
+            'max_depth': 3,
             # # 'max_leaf_nodes': 4
         },
         AdaBoostClassifier: {
             # 'n_estimators': 5000,
             # # 'learning_rate': 0.1,
         },
+        RandomForestClassifier: {
+            'n_estimators': 5000,
+            'max_depth': 3,
+        },
     }
+    # model_class = XGBClassifier
     model_class = GradientBoostingClassifier
     # model_class = AdaBoostClassifier
     # model_class = RandomForestClassifier
@@ -168,7 +180,10 @@ if __name__ == '__main__':
         ch.setLevel(logging.DEBUG)
         _, scores = train(args.training_set_file, model_class, MODEL_PARAMS.get(model_class, {}))
         logger.info('%s - Score: %f+/-%f %s', model_class.__name__, scores.mean(), scores.std(), scores)
-        # search_xgboost_hyperparams(args.training_set_file)
     elif args.command == 'predict':
         pipeline, _ = train(args.training_set_file, model_class, MODEL_PARAMS.get(model_class, {}))
         predict(pipeline, args.predict_file)
+    elif args.command == 'search':
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+        search_xgboost_hyperparams(args.training_set_file)
