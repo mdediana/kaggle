40b076f6e25fb4c62b89383393de8d17816e91db
Mauricio De Diana <mdediana@gmail.com>
2020-05-21 12:41:15 -0300

    Revert "Clean up"
    This reverts commit 8746fe4e7fded4da8c55b723f23541202434ded1.

--- mnist/model.py
+++ mnist/model.py
@@ -7,10 +7,12 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Flatten, Dense, Dropout, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Flatten, InputLayer, Dense, Dropout, Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import SGD
+from kerastuner.tuners import RandomSearch
 
 
 NUM_INSTANCES = 42000
@@ -22,8 +24,17 @@ NUM_CLASSES = 10
 NUM_EPOCHS = 200
 BATCH_SIZE = 16
 VALIDATION_SPLIT = 0.1
+SCORING = 'accuracy'
+ACTIVATION = 'selu'
+KERNEL_INITIALIZER = 'lecun_uniform'
 MC_SAMPLES = 100
 TENSORBOARD_LOG_DIR = os.path.join(os.curdir, 'logs/tensorboard', datetime.now().isoformat())
+KERASTUNER_DIR = os.path.join(os.curdir, 'logs/kerastuner')
+BEST_PARAMS = dict(
+    n_neurons=[385, 257, 449],  # Neurons on each hidden layer
+    learning_rate=0.003,
+    dropout_rate=0.1,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -38,6 +49,24 @@ def _read_test_csv(filename):
     X /= 255.0   # Scale pixels (min 0, max 255)
     X = X.values.reshape(len(X), IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS)  # X turned into a ndarray
     return X
+
+
+def _read_csv(filename, is_2d=True, split_X_y=True):
+    """Return X, y if training data or X, None if test data"""
+    df = pd.read_csv(filename)
+    if split_X_y and 'label' in df:
+        X = df.drop(columns=['label'])
+        y = to_categorical(df['label'])
+    else:
+        X = df
+        y = None
+    # X /= 255.0   # Scale pixels (min 0, max 255)
+    X = tf.dtypes.cast(X, tf.uint16)
+    if is_2d:
+        X = X.values.reshape(len(X), IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS)  # X turned into a ndarray
+    else:
+        X = X.values
+    return X, y
 
 
 def _read_tfrecords(training_set_file, ds_type=None):
@@ -88,9 +117,17 @@ def _build_model(learning_rate=3e-3):  # Score: 0.99171 (shuffle datasets)
         Dense(NUM_CLASSES, activation='softmax')
     ])
     optimizer = SGD(lr=learning_rate, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=[SCORING])
     model.summary()
     return model
+
+
+def _build_model_tuner(hp):
+    n_neurons = [hp.Int('units_' + str(i), min_value=1, max_value=512, step=32)
+                 for i in range(hp.Int('num_layers', 0, 5))]
+    learning_rate = hp.Choice('learning_rate', [1e-3, 3e-3, 5e-3])
+    dropout_rate = hp.Choice('dropout_rate', [0.05, 0.1, 0.15])
+    return _build_model_dense(n_neurons, learning_rate, dropout_rate)
 
 
 def train(training_set_file, test_set_file, model_file=None):
@@ -122,17 +159,40 @@ def predict(training_set_file, test_set_file, model_file=None, output_file=None)
     X_test = _read_test_csv(test_set_file)
     logger.info('X test shape: %s', X_test.shape)
     logger.info('Predict using mode of %d Monte Carlo runs', MC_SAMPLES)
-    preds_i = list()
-    for i in range(MC_SAMPLES):
-        preds_i.append(np.argmax(model.predict(X_test), axis=-1))
-        logger.info('Run finished: %d', i + 1)
-    preds = stats.mode(np.stack(preds_i)).mode.flatten()
-    out_df = pd.DataFrame({"ImageId": range(1, len(preds) + 1), "Label": preds})
+    # TODO: WARNING:tensorflow:From model.py:186: Sequential.predict_classes (from
+    # tensorflow.python.keras.engine.sequential) is deprecated and will be removed after 2021-01-01.
+    # Instructions for updating:
+    # Please use instead:* `np.argmax(model.predict(x), axis=-1)`,   if your model does multi-class classification
+    # (e.g. if it uses a `softmax` last-layer activation).* `(model.predict(x) > 0.5).astype("int32")`,
+    # if your model does binary classification   (e.g. if it uses a `sigmoid` last-layer activation).
+    ys = np.stack([model.predict_classes(X_test) for i in range(MC_SAMPLES)])  # TODO: Use loop to print progress
+    y = stats.mode(ys).mode.flatten()
+    out_df = pd.DataFrame({"ImageId": range(1, len(y) + 1), "Label": y})
     if output_file is not None:
         out_df.to_csv(output_file, index=False)
     else:
         logger.info('No output file set, number of predictions: %d', len(out_df))
     logger.info('Done')
+
+
+def search_params(training_set_file, test_set_file):
+    X_train, y_train, _ = _read_data(training_set_file, test_set_file)
+    tuner = RandomSearch(_build_model_tuner,
+                         objective='val_accuracy',
+                         max_trials=5,
+                         executions_per_trial=2,
+                         directory=KERASTUNER_DIR,
+                         project_name='mnist')
+    tuner.search_space_summary()
+    tuner.search(X_train, y_train,
+                 epochs=NUM_EPOCHS,
+                 batch_size=BATCH_SIZE,
+                 validation_split=VALIDATION_SPLIT,
+                 verbose=2)
+    tuner.results_summary()
+    model = tuner.get_best_models(num_models=1)[0]
+    logger.info('Best model: ')
+    model.summary()
 
 
 def _bytes_feature(value):
@@ -174,7 +234,7 @@ def csv_to_tfrecords(input_file, output_file):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Recognize digits in the MNIST dataset')
     parser.add_argument('command', help='Command to run',
-                        choices=['train', 'predict', 'csv-to-tfrecords'],
+                        choices=['train', 'predict', 'search-params', 'csv-to-tfrecords'],
                         default='train')
     parser.add_argument('--training-set-file', help='Training set file', default='train.tf_records')
     parser.add_argument('--test-set-file', help='Test set file', default='test.csv')
@@ -195,5 +255,7 @@ if __name__ == '__main__':
         train(args.training_set_file, args.test_set_file, args.model_file)
     elif args.command == 'predict':
         predict(args.training_set_file, args.test_set_file, args.model_file, args.output_file)
+    elif args.command == 'search-params':
+        search_params(args.training_set_file, args.test_set_file)
     elif args.command == 'csv-to-tfrecords':
         csv_to_tfrecords(args.training_set_file, args.output_file)
